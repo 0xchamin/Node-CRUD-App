d3f1b66680af4bf20186629569d7dca61b36d792
0xchamin <chmk90@gmail.com>
2018-04-02 19:46:41 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+//load routes
+const ideas = require('./routes/ideas');//
+const users = require('./routes/users');//users
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  //useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Static folders
+//set "public" folder to be express static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method override middleware
+app.use(methodOverride('_method'));
+
+// Express-Session middleware
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+	//cookie: {secure : true}
+}));
+
+//Middleware for connect-flash
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.err_msg = req.flash('err_msg');
+	res.locals.error = req.flash('error');
+	next();//call next piece of middleware
+});
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = 5000;
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
+});
